c05c14ca1f2e71f46da4799c34488f85d493a886
Marijan Limov <marijan.limov@gmail.com>
2023-09-10 17:22:34 +0200

    Use the correct compression algorithm and switch from magiskboot to mkbootimg

--- raven-build-and-run.sh
+++ raven-build-and-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pstore=""
-debug="-r DEBUG"
+debug="DEBUG"
 
 while getopts ":hp" option; do
   case $option in
@@ -12,19 +12,9 @@ while getopts ":hp" option; do
   esac
 done
 
-rm tools/magiskboot/kernel
-rm boot-raven.img
 rm -r logs
 
 ./build.sh -f -r $debug $pstore -d raven || { echo "Build failed"; exit 1; }
-rm boot-raven.img
-
-cp workspace/Build/raven/DEBUG_CLANG38/FV/TENSOR_UEFI.fd-bootshim tools/magiskboot/kernel
-
-cd tools/magiskboot
-./magiskboot_x86 repack boot_a
-cd ../..
-mv tools/magiskboot/new-boot.img boot-raven.img 
 
 fastboot oem ramdump wipe_file kernel.log
 
